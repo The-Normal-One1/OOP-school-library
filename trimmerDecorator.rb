62817e0b75499d0d7be1529fa43c47a885f04e26
Ermi <manermidem@gmail.com>
2023-04-11 17:31:43 +0300

    Add a method correct_name to trim the words.

--- trimmerDecorator.rb
+++ trimmerDecorator.rb
@@ -2,4 +2,12 @@ require './base_decorator'
 
 class TrimmerDecorator < Decorator
 
+
+    def correct_name
+        if @nameable.correct_name.length < 10
+            @nameable.correct_name
+        else
+           "#{@nameable.correct_name[0..9]}..." 
+        end
+    end
 end
