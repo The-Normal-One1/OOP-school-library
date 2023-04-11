0af0933261bbc3361365b99106565b26d617ec0c
Ermi <manermidem@gmail.com>
2023-04-11 17:51:47 +0300

    Modify the file

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -4,7 +4,7 @@ class TrimmerDecorator < Decorator
 
 
     def correct_name
-        if @nameable.correct_name.length < 10
+        if @nameable.correct_name.length <= 10
             @nameable.correct_name
         else
            "#{@nameable.correct_name[0..9]}..." 
