165dca9a14e7f0bab5ab84be6703f7e348e3b724
Ermi <manermidem@gmail.com>
2023-04-13 14:24:09 +0300

    Add a method to list all people

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require "./book.rb"
+require "./person.rb"
 
 class App
     def list_all_books(books, show_list: false)
         puts "No list of books are found" if books.empty?
         books.each do |book, list|
             puts "#{"#{list})" if show_list} Title: #{book.title}, Author: #{book.author}"
+        end
+        puts '\n'
+    end
+
+    def list_all_people(people, show_list: false)
+        puts "No list of people are found" if people.empty?
+        people.each do |person, list|
+            puts "#{"#{list})" if show_list} [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
         end
         puts '\n'
     end
