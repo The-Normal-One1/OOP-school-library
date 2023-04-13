9a5f23a383822715444b9c003b7a4fe8dbc3af03
Ermi <manermidem@gmail.com>
2023-04-13 08:27:45 +0300

    Create the 'has-many' side relationship.

--- class_room.rb
+++ class_room.rb
@@ -1,6 +1,13 @@
 class Classroom
     attr_accessor :lable
+    attr_reader :students
     def initialize(lable)
         @lable = lable
+        @students = []
+    end
+
+#classroom has many students
+    def add_student(student)
+        @students.push(student)
     end
 end
