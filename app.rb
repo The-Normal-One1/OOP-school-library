75d71d498b1dd414849ba301f8b08a1881d1f79d
Ermi <manermidem@gmail.com>
2023-04-13 17:26:33 +0300

    Edit the file

--- app.rb
+++ app.rb
@@ -6,18 +6,18 @@ require './student.rb'
 class App
     def list_all_books(books, show_list: false)
         puts "No list of books are found" if books.empty?
-        books.each do |book, list|
+        books.each_with_index do |book, list|
             puts "#{"#{list})" if show_list} Title: #{book.title}, Author: #{book.author}"
         end
-        puts '\n'
+        puts "\n"
     end
 
     def list_all_people(people, show_list: false)
         puts "No list of people are found" if people.empty?
-        people.each do |person, list|
+        people.each_with_index do |person, list|
             puts "#{"#{list})" if show_list} [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
         end
-        puts '\n'
+        puts "\n"
     end
 
     def create_person(select, age, name,specialization, parent_permission)
@@ -43,6 +43,6 @@ class App
         person.each do |person, list|
             puts "Rentals: #{person.rentals}"
         end
-        puts '\n'
+        puts "\n"
     end
 end
