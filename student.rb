ca364022edc2acd419819f42939245ab402770eb
Ermi <manermidem@gmail.com>
2023-04-13 09:46:00 +0300

    Edit student class

--- student.rb
+++ student.rb
@@ -1,5 +1,7 @@
 require_relative 'person'
 require './class_room.rb'
+require './book.rb'
+require './rental.rb'
 
 class Student < Person
   attr_accessor :classroom
@@ -44,3 +46,14 @@ puts classroom1.students.map { |student| student.name}
 # A student belongs to classroom
 puts classroom1.students[0].classroom
 puts classroom1.students[1].classroom
+
+book1 = Book.new('The Alchemist', 'Paulo Coelho')
+book2 = Book.new('The Little Prince', 'Antoine de Saint-Exupéry')
+
+rental1 = Rental.new('2021-01-01', book1, student1)
+rental2 = Rental.new('2021-01-02', book2, student2)
+
+puts rental1.book
+puts rental1.person
+puts rental2.book
+puts rental2.person
