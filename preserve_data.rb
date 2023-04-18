44fff43b2371d2185c8dbc2b68836770f7ebf678
Ermi <manermidem@gmail.com>
2023-04-18 16:21:30 +0300

    Create rentals.json file

--- preserve_data.rb
+++ preserve_data.rb
@@ -7,3 +7,4 @@ base = "#{Dir.pwd}/stored_data"
 
 File.open("#{base}/books.json", "w") unless File.exist?("#{base}/books.json")
 File.open("#{base}/people.json", "w") unless File.exist?("#{base}/people.json")
+File.open("#{base}/rentals.json", "w") unless File.exist?("#{base}/rentals.json")
