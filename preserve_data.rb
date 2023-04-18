fb47170502d8751dc5ac18092f702354f83a0353
Ermi <manermidem@gmail.com>
2023-04-18 16:16:46 +0300

    Create books.json file inside store_data directory

--- preserve_data.rb
+++ preserve_data.rb
@@ -2,3 +2,7 @@ require 'fileutils'
 require 'json'
 
 FileUtils.mkdir_p('stored_data')
+
+base = "#{Dir.pwd}/stored_data"
+
+File.open("#{base}/books.json", "w") unless File.exist?("#{base}/books.json")
