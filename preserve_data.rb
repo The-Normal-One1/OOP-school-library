a24eafbac52f987c33058749472a1581c1dbf0c5
Ermi <manermidem@gmail.com>
2023-04-18 16:20:25 +0300

    Create people.json file inside stored_data dir

--- preserve_data.rb
+++ preserve_data.rb
@@ -6,3 +6,4 @@ FileUtils.mkdir_p('stored_data')
 base = "#{Dir.pwd}/stored_data"
 
 File.open("#{base}/books.json", "w") unless File.exist?("#{base}/books.json")
+File.open("#{base}/people.json", "w") unless File.exist?("#{base}/people.json")
