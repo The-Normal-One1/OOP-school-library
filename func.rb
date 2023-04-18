18c2b88fbe276c2f0d645743050a862ed060c95b
Ermi <manermidem@gmail.com>
2023-04-18 15:30:02 +0300

    Add list options in func.rb file

--- func.rb
+++ func.rb
@@ -8,4 +8,19 @@ class Func
     @list_of_books = []
     @list_of_people = []
   end
+
+  # list of options
+  def options
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    gets.chomp.to_i
+  end
+
+  
 end
